09b04a8704791cfb0913db793f1e17a4e124ec68
Maihiranabi786 <55278414+Maihiranabi786@users.noreply.github.com>
2020-01-08 12:34:20 +0530

    Update createparkslot.test.js

--- createparkslot.test.js
+++ createparkslot.test.js
@@ -1,9 +1,11 @@
-let expect= require('chai').expect;
-let parkingLot = require ('../parkingLot');
-describe('create_parking-lot 6', async function (){
-it('should create 6 parking slots', async function (){
+let expect = require('chai').expect;
+let parkingLot = require('../parkingLot');
+describe('create_parking_lot 6', async function ()
+{
+it('should create 6 parking slots', async function () {
 var preResult = 'created a parking lot with 6 slots';
-var result = await parkingLot.createparkingLot(6);
+var result = await 
+parkingLot.createparkingLot(6);
 console.log(result);
 expect(result).to.be.equal(preResult);
 });
